a6077ac361345bc3654bfddf9e13c7f946b93573
Realmzer <therealm4mc@gmail.com>
2023-03-04 09:59:14 -0500

    Source Code Release

---
+++ source/options/ModOptionsSubState.hx
@@ -0,0 +1,52 @@
+package options;
+
+#if desktop
+import Discord.DiscordClient;
+#end
+import flash.text.TextField;
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.addons.display.FlxGridOverlay;
+import flixel.group.FlxGroup.FlxTypedGroup;
+import flixel.math.FlxMath;
+import flixel.text.FlxText;
+import flixel.util.FlxColor;
+import lime.utils.Assets;
+import flixel.FlxSubState;
+import flash.text.TextField;
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.util.FlxSave;
+import haxe.Json;
+import flixel.tweens.FlxEase;
+import flixel.tweens.FlxTween;
+import flixel.util.FlxTimer;
+import flixel.input.keyboard.FlxKey;
+import flixel.graphics.FlxGraphic;
+import Controls;
+
+using StringTools;
+
+class ModOptionsSubState extends BaseOptionsMenu
+{
+	public function new()
+	{
+		title = 'Mod Options';
+		rpcTitle = 'Mod Options Menu'; //for Discord Rich Presence
+
+		var option:Option = new Option('Character Select NOT WORKING',
+		'Press Enter to disable character select. \n Can get annoying sometimes when its turned on it can get laggy.',
+		'CharMenu',
+		'bool',
+		false);
+	    //addOption(option);
+
+
+		super();
+	}
+
+	function onChangeHitsoundVolume()
+	{
+		FlxG.sound.play(Paths.sound('hitsound'), ClientPrefs.hitsoundVolume);
+	}
+}
